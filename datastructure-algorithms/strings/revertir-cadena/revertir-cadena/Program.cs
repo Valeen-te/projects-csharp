d90f50d2e3e3357d826c0ad745a3355f8e4518e7
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-22 14:56:55 -0400

    create revertir-cadena

---
+++ datastructure-algorithms/strings/revertir-cadena/revertir-cadena/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Dame una palabra");
+string? cadena = Console.ReadLine();
+
+string resultado = "";
+
+for(int i = cadena.Length - 1; i >= 0;i--)
+{
+    resultado += cadena[i];
+}
+
+Console.WriteLine($"\nPalabra invertida: {resultado}");
