47c0973019fbf8192ff58ad242334488aedbb813
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-22 14:57:32 -0400

    create subcadenas-a-cadenas

---
+++ datastructure-algorithms/strings/subcadenas-a-cadenas/subcadenas-a-cadenas/Program.cs
@@ -0,0 +1,12 @@
+﻿string cadena = "srtuvwxyz";
+
+for (int i = 1; i <= cadena.Length; i++)
+{
+    
+    for (int j = 0; j < cadena.Length - i; j++)
+    {
+        string subcadena = cadena.Substring(j,i);
+        
+        Console.WriteLine(subcadena);
+    }
+}
