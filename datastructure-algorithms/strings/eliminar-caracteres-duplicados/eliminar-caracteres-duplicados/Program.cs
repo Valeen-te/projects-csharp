741e0f300236e18b4cd61e6f3218918f17069784
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-22 14:56:02 -0400

    create eliminar-caracteres-duplicados

---
+++ datastructure-algorithms/strings/eliminar-caracteres-duplicados/eliminar-caracteres-duplicados/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] palabras = new string[]
+{
+    "CsharpCurso",
+    "Google",
+    "Yahoo",
+    "CNN" 
+};
+
+foreach (string palabra in palabras)
+{
+    Console.WriteLine(palabra);
+
+    string resultado = "";
+
+    foreach (char s in palabra)
+    {
+        if (!resultado.Contains(s))
+        {
+            resultado += s;
+        }
+    }
+
+    Console.WriteLine(resultado);
+    Console.WriteLine();
+}
