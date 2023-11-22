a6036d73e411bed8e6ea9292c8bf00cbdb71be3e
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-22 14:53:55 -0400

    create contar-palabras

---
+++ datastructure-algorithms/strings/contar-palabras/contar-palabras/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Ingrese una frase:");
+string frase = Console.ReadLine();
+
+string[] palabras = frase.Split(" ");
+
+Console.WriteLine($"\nLa frase tiene {palabras.Length} palabras");
