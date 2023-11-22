0509b6501db73c2b8b065e0661303f4ebe4a93ae
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-22 14:50:53 -0400

    create cadena-palindromo

---
+++ datastructure-algorithms/strings/cadena-palindromo/cadena-palíndromo/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Ingrese una palabra:");
+string cadena = Console.ReadLine().ToLower();
+
+string cadenaInversa = "";
+
+for(int i = cadena.Length - 1;  i >= 0; i--)
+{
+    cadenaInversa += cadena[i];
+}
+
+Console.WriteLine();
+
+if(cadena == cadenaInversa)
+{
+    Console.WriteLine($"{cadena} es un palíndromo");
+} else
+    Console.WriteLine($"{cadena} no es un palíndromo");
