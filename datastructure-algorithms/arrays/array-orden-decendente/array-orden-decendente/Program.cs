257f070f2685200ecc8e43c443ed345046fc6257
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-23 23:35:16 -0400

    create array-orden-decendente

---
+++ datastructure-algorithms/arrays/array-orden-decendente/array-orden-decendente/Program.cs
@@ -0,0 +1,18 @@
+﻿char[] letras = new char[] { 'a','c','f','h','b','e','d','g','j','i' };
+
+ImprimirArray(letras);
+
+Array.Sort(letras);
+Array.Reverse(letras);
+
+Console.WriteLine();
+
+ImprimirArray(letras);
+
+static void ImprimirArray(char[] letras)
+{
+    foreach (char c in letras)
+    {
+        Console.Write($"{c} ");
+    }
+}
