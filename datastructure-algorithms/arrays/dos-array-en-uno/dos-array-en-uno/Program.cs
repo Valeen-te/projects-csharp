8896fd29a7dff7937a9a26795cd5f5d01ea106db
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-23 23:35:56 -0400

    create dos-array-en-uno

---
+++ datastructure-algorithms/arrays/dos-array-en-uno/dos-array-en-uno/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] array1 = { 5, 6, 7, 8, 9 };
+int[] array2 = { 0, 1, 2, 3, 4, 0, 0, 0, 0, 0 };
+
+ImprimirArray(array1);
+ImprimirArray(array2);
+
+int numMax = array1.Length;
+int posicion = 0;
+
+for (int i = numMax; i < array2.Length; i++)
+{
+    array2[i] = array1[posicion];
+    posicion++;
+}
+
+ImprimirArray(array2);
+
+static void ImprimirArray(int[] array)
+{
+    foreach (int i in array) Console.Write(i);
+
+    Console.WriteLine("\n");
+}
+
