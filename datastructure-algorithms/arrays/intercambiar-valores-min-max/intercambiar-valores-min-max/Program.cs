3a44aba5b427fb17b691db347e35b8fbfafa5b6f
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-24 14:34:13 -0400

    create intercambiar-valores-min-max

---
+++ datastructure-algorithms/arrays/intercambiar-valores-min-max/intercambiar-valores-min-max/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+int[] array = { 5,9,6,8,3,2,4,1,7 };
+
+Console.WriteLine("Array inicial:");
+
+ImprimirArray(array);
+
+int indiceMinimo = 0;
+
+int indiceMaximo = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < array[indiceMinimo])
+        indiceMinimo = i;
+
+    if (array[i] > array[indiceMaximo])
+        indiceMaximo = i;
+}
+
+int temp = array[indiceMinimo];
+
+array[indiceMinimo] = array[indiceMaximo];
+
+array[indiceMaximo] = temp;
+
+Console.WriteLine("Array final:");
+
+
+ImprimirArray(array);
+
+
+
+static void ImprimirArray(int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
