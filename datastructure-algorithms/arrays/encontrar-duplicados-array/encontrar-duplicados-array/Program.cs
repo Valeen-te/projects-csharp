15f579e2b121116c867bf4a25217607f02730f63
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-25 11:51:01 -0400

    create encontrar-duplicados-array

---
+++ datastructure-algorithms/arrays/encontrar-duplicados-array/encontrar-duplicados-array/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] array = {4,2,5,2,6,4,7,8,9,3,7 };
+
+Console.WriteLine("Array:");
+
+ImprimirArray(array);
+
+EncontrarDuplicados(array);
+
+
+static void ImprimirArray(int[] array)
+{
+    foreach( int i in array )
+        Console.Write( i + " " );
+    Console.WriteLine();
+}
+
+
+static void EncontrarDuplicados (int[] array )
+{
+    for( int i = 0; i < array.Length; i++)
+    {
+        for( int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] == array[i])
+            {
+                Console.WriteLine($"El valor {array[j]} está duplicado");
+            }
+        }
+    }
+}
