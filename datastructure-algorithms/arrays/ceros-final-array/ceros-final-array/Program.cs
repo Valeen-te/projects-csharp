3173276cd8afd07ee47a12032cf37f9ddb067d21
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-25 00:28:16 -0400

    create ceros-final-array

---
+++ datastructure-algorithms/arrays/ceros-final-array/ceros-final-array/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] array = { 9, 7, 5, 0, 3, 4, 0, 6, 1, 2, 0, 8 };
+
+Console.WriteLine("Array inicial:");
+ImprimirArray(array);
+
+MoverCeros(array);
+
+Console.WriteLine("Array final:");
+ImprimirArray(array);
+
+
+static void ImprimirArray(int[] array)
+{
+    foreach ( int i in array)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+
+static void MoverCeros(int[] array)
+{
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        if (array[i] == 0)
+        {
+            for (int j = i; j < array.Length - 1; j++)
+            {
+                int temp = array[j + 1];
+
+                array[j + 1] = array[j];
+
+                array[j] = temp;
+            }
+        }
+    }
+}
