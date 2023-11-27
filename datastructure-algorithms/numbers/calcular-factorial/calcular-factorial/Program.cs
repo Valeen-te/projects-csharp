13816e84704247a198e5c88886417704a88b23b5
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-27 12:03:57 -0400

    create calcular-factorial

---
+++ datastructure-algorithms/numbers/calcular-factorial/calcular-factorial/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("ingrese el número: ");
+int num = int.Parse(Console.ReadLine());
+
+int factorial = num;
+
+for (int i = 1; i < num; i++)
+{
+    factorial = factorial * i;
+}
+
+Console.WriteLine($"El factorial de {num} es: {factorial}");
