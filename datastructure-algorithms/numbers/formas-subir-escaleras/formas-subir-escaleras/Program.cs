6359f1a696a5098d8f791ada6acf0227b4a57565
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-26 23:57:19 -0400

    create formas-subir-escaleras

---
+++ datastructure-algorithms/numbers/formas-subir-escaleras/formas-subir-escaleras/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Timers;
+
+Console.Write("Dime el número de escaleras: ");
+
+int escaleras = int.Parse(Console.ReadLine());
+
+int a = 0;
+
+int b = 1;
+
+int c = 0;
+
+for(int i = 1; i <= escaleras; i++)
+{
+    c = a + b;
+
+    if (i != 1)
+    {
+        b = a;
+    }
+
+    a = c;
+}
+
+Console.WriteLine($"\nEl número de formas de subir las escaleras es: {c}");
