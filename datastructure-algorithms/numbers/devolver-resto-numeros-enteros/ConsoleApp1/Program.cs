8cee4009f75461d24c0093dcae60590adf8e5fb8
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-28 09:41:03 -0400

    create devolver-resto-numeros-enteros

---
+++ datastructure-algorithms/numbers/devolver-resto-numeros-enteros/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Dime el dividendo: ");
+
+int dividendo = int.Parse(Console.ReadLine());
+
+Console.Write("Dime el divisor: ");
+
+int divisor = int.Parse(Console.ReadLine());
+
+ObtenerResiduo(dividendo, divisor);
+
+static void ObtenerResiduo(int x, int y)
+{
+    if(y == 0)
+    {
+        Console.WriteLine("El dividendo no puede ser 0");
+    }else if(x < y)
+    {
+        Console.WriteLine("El dividendo no puede ser menor que el divisor");
+    }
+    else
+    {
+        Console.WriteLine($"El resto de {x} entre {y} es: " + x % y);
+    }
+    
+}
