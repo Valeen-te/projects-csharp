4cb2341c91434dc65ac061a27ae5ce0ad354d6d7
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-28 14:25:17 -0400

    create numero-armstrong

---
+++ datastructure-algorithms/numbers/numero-armstrong/numero-armstrong/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Ingresa un número: ");
+string nroArmstrong = Console.ReadLine();
+
+char[] numeros = nroArmstrong.ToCharArray();
+double resultado = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    int temp;
+    int.TryParse(numeros[i].ToString(), out temp);
+    resultado += Math.Pow(temp, numeros.Length);
+}
+
+if (resultado == int.Parse(nroArmstrong))
+{
+    Console.WriteLine($"El número {nroArmstrong} es un número de Armstrong.");
+}
+else
+{
+    Console.WriteLine($"El número {nroArmstrong} no es un número de Armstrong.");
+}
