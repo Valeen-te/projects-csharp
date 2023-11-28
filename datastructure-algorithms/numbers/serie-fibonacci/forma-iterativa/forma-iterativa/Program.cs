be65852e355d9cdbb09b6207e5d3458285b49802
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-28 00:43:38 -0400

    create serie-fibonacci

---
+++ datastructure-algorithms/numbers/serie-fibonacci/forma-iterativa/forma-iterativa/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Ingrese la longitud de la serie fibonacci: ");
+
+int longitud = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nLa serie fibonnacci es:");
+
+int a = 0, b = 0, r = 0;
+
+for (int i = 0; i < longitud; i++)
+{
+    if(i == 1)
+    {
+        b = 1;
+    }
+
+    r = a + b;
+
+    b = a;
+
+    a = r;
+
+    Console.Write(r + " ");
+}
