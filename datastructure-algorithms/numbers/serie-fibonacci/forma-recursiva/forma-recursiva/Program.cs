873c5449bd49162075681b7e1cf689078909371b
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-28 01:17:55 -0400

    add forma-recursiva

---
+++ datastructure-algorithms/numbers/serie-fibonacci/forma-recursiva/forma-recursiva/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Ingresa la longitud de la serie Fibonacci: ");
+
+int Longitud = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nLa longitud de la serie Fibonacci es: ");
+
+for (int i = 0; i < Longitud; i++)
+{
+    Console.Write(CalcularFibacci(7) + " ");
+}
+
+static int CalcularFibacci(int longitud)
+{
+    if (longitud < 2)
+    {
+        Console.Write(longitud);
+        return longitud;
+    }
+    else
+    {
+        return CalcularFibacci(longitud - 1) + CalcularFibacci(longitud - 2);
+    }
+}
