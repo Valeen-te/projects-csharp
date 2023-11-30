2de5b415f6a46621d85c4bccce590bec2972f683
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-29 21:36:03 -0400

    create calcular-mcd-mcm

---
+++ datastructure-algorithms/numbers/calcular-mcd-mcm/Recursividad/Recursividad/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("C# Programa para MCD y MCM");
+
+Console.Write("Dame el primer número: ");
+
+int primero = int.Parse(Console.ReadLine());
+
+Console.Write("Dame el segundo número: ");
+
+int segundo = int.Parse(Console.ReadLine());
+
+int mcd = CalcularMcd(primero, segundo);
+
+int mcm = CalcualrMcm(primero, segundo);
+
+Console.WriteLine($"\nEl MCD entre {primero} y {segundo} es: {mcd}");
+Console.WriteLine($"El MCM entre {primero} y {segundo} es: {mcm}");
+
+
+static int CalcularMcd(int primero, int segundo)
+{
+    if (primero > segundo)
+        primero = primero % segundo;
+    else
+        segundo = segundo % primero;
+
+    if (segundo == 0)
+    {
+        return primero;
+    }
+    else if (primero == 0)
+    {
+        return segundo;
+    }
+    else 
+    { 
+        return CalcularMcd(primero,segundo);
+    }
+}
+
+static int CalcualrMcm(int primero, int segundo)
+{
+    return (primero * segundo) / CalcularMcd(primero, segundo);
+}
---
+++ datastructure-algorithms/numbers/calcular-mcd-mcm/bucle/bucle-mcd-mcm/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("C# Programa para MCD y MCM");
+
+Console.Write("Dame el primer número: ");
+
+int primero = int.Parse(Console.ReadLine());
+
+Console.Write("Dame el segundo número: ");
+
+int segundo = int.Parse(Console.ReadLine());
+
+int mcd = ConsegirMcd(primero, segundo);
+
+int mcm = ConsegirMcm(primero, segundo);
+
+Console.WriteLine();
+Console.WriteLine($"MCD de {primero} y {segundo}: {mcd}");
+Console.WriteLine($"MCM de {primero} y {segundo}: {mcm}");
+
+
+static int ConsegirMcd(int primero, int segundo)
+{
+    while (primero != segundo)
+    {
+        if (primero > segundo)
+            primero -= segundo;
+
+        if (primero < segundo)
+            segundo -= primero;
+    }
+
+    return primero;
+}
+
+static int ConsegirMcm(int primero, int segundo)
+{
+    return (primero * segundo) / ConsegirMcd(primero, segundo);
+}
