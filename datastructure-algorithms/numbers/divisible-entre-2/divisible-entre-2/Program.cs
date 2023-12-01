ce32afc37436402922351ff8e419a10c75ec7465
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-01 17:15:07 -0400

    create divisible-entre-2

---
+++ datastructure-algorithms/numbers/divisible-entre-2/divisible-entre-2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ingrese un número: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("Es divisible entre 2");
+}
+else
+{
+    Console.WriteLine("No es divisible entre 2");
+}
