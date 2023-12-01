58c4e167bbbc0cc67082558503c150a2b828a10d
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-01 18:30:37 -0400

    create calculos-aritmeticos

---
+++ datastructure-algorithms/numbers/calculos-aritmeticos/calculos-aritmeticos/Program.cs
@@ -0,0 +1,49 @@
+﻿// Seleccionar operación
+
+Console.WriteLine("Opciones");
+Console.WriteLine("1) sumar\n2) restar\n3) multiplicar\n4) dividir");
+
+Console.Write("\nSelecciona una opción: ");
+
+int opcion;
+
+int.TryParse(Console.ReadLine(), out opcion);
+
+// Verificar si es una opcion valida
+
+if (opcion > 0 && opcion < 5)
+{
+    //Ingresar valores a calcular
+
+    Console.Write("\nDame el primer número: ");
+
+    int a = int.Parse(Console.ReadLine());
+
+    Console.Write("Dame el segundo número: ");
+
+    int b = int.Parse(Console.ReadLine());
+
+    //Mostrar operación
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine($"\n {a} + {b} = " + (a + b));
+            break;
+        case 2:
+            Console.WriteLine($"\n {a} - {b} = " + (a - b));
+            break;
+        case 3:
+            Console.WriteLine($"\n {a} * {b} = " + (a * b));
+            break;
+        case 4:
+            Console.WriteLine($"\n {a} / {b} = " + (a / b));
+            break;
+    }
+
+}
+else
+{
+    Console.WriteLine("No exite esa opcion");
+}
+
