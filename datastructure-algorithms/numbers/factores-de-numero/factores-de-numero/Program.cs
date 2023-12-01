998d3be1abb4dfac1c402fb059d230c0cb907367
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-01 18:47:59 -0400

    create factores-de-numero

---
+++ datastructure-algorithms/numbers/factores-de-numero/factores-de-numero/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Ingrese un número: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\nFactores: ");
+for (int i = 1; i <= numero; i++)
+{
+    if(numero % i == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
