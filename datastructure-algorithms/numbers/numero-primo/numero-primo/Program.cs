e68c1608f0d09100645239b6fd8ac34df04e48ec
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-11-30 20:48:22 -0400

    create numero-primo

---
+++ datastructure-algorithms/numbers/numero-primo/numero-primo/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Dame un número: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+int contador = 0;
+
+for (int i = numero - 1; i > 0; i--)
+{
+    if (numero % i == 0)
+    {
+        contador++;
+    }
+}
+
+if(contador > 1)
+{
+    Console.WriteLine($"\nEl número {numero} no es primo");
+}
+else
+{
+    Console.WriteLine($"\nEl número {numero} es primo");
+}
