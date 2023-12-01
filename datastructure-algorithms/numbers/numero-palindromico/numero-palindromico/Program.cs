dd4d09cc04cb29d7a9ee64220a38e20bbfd122f1
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-01 14:23:36 -0400

    update .gitignore and delete obj, bin folders

---
+++ datastructure-algorithms/numbers/numero-palindromico/numero-palindromico/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Ingrese un número: ");
+
+string numero = Console.ReadLine();
+
+char[] numeroChar = numero.ToCharArray();
+
+string? resultado = null;
+
+for (int i = numeroChar.Length -1 ; i > -1; i--)
+{
+    int num;
+
+    int.TryParse(numeroChar[i].ToString(),out num);
+
+    resultado += numeroChar[i];
+}
+
+if  (resultado == numero)
+{
+    Console.WriteLine($"El número {numero} es palindrómico");
+}
+else
+{
+    Console.WriteLine($"El número {numero} no es palindrómico");
+}
