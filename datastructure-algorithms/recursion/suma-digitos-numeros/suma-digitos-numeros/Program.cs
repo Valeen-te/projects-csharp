aaef7d5d6b5ca4e9b1eda9b5be920e5066c6e0c5
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-02 01:00:16 -0400

    create suma-digitos-numeros

---
+++ datastructure-algorithms/recursion/suma-digitos-numeros/suma-digitos-numeros/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int num, result;
+
+Console.Write("Ingresa un número: ");
+
+num = int.Parse(Console.ReadLine());
+
+result = SumaDigitos(num);
+
+Console.WriteLine($"La suma de los digitos de {num} es {result}");
+
+static int SumaDigitos(int num)
+{
+    if (num < 10)
+    {
+        return num;
+    }
+    else
+    {
+        return num % 10 + SumaDigitos(num/10);
+    }
+}
