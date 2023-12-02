f79300c4973922a08048b4f395dd44e62febd128
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-02 12:49:50 -0400

    create cacular-potencia

---
+++ datastructure-algorithms/recursion/calcular-potencia/calcular-potencia/Program.cs
@@ -0,0 +1,35 @@
+﻿int num, exponente;
+double? resultado;
+
+Console.WriteLine("Calculadora de potencias\n");
+
+Console.WriteLine("Ingrese un número:");
+
+num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese su exponente:");
+
+exponente = int.Parse(Console.ReadLine());
+
+resultado = CalcularPotencia(num, exponente);
+
+if(resultado != null)
+{
+    Console.WriteLine($"\nLa potencia de {num}^{exponente} es: {resultado}");
+}
+
+static double? CalcularPotencia(int num, int exponente)
+{
+    switch (exponente)
+    {
+        case < 0:
+            Console.WriteLine("\nSolo se admiten exponentes positivos");
+            return null;
+        case 1:
+            return num;
+        case 0:
+            return 1;
+        default:
+            return num * CalcularPotencia(num, exponente - 1);
+    }
+}
