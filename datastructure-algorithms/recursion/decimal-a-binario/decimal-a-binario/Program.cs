b015733f32e2747fc0f2a9a8004f3a66f5febc61
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-02 11:29:19 -0400

    create decimal-a-binario

---
+++ datastructure-algorithms/recursion/decimal-a-binario/decimal-a-binario/Program.cs
@@ -0,0 +1,22 @@
+﻿int num;
+string resultado;
+
+Console.Write("Ingrese un número: ");
+
+num = int.Parse(Console.ReadLine());
+
+resultado = ObterneBinario(num);
+
+Console.WriteLine($"El binario equivalente al número es: {resultado}");
+
+static string ObterneBinario(int num)
+{
+    if (num == 1)
+    {
+        return num.ToString();
+    }
+    else
+    {
+        return ObterneBinario(num / 2) + (num % 2);
+    }
+}
