358e66605ab6fc7ff41b98e3eba83cb94df4e0d3
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-03 23:21:33 -0400

    create busqueda-binaria

---
+++ datastructure-algorithms/recursion/busqueda-binaria/busqueda-binaria/Program.cs
@@ -0,0 +1,49 @@
+﻿int? buscar = null, posicion = null;
+
+Console.WriteLine("ingresa la longitud de la cadena:");
+
+int[] listaNumeros = new int[int.Parse(Console.ReadLine())];
+
+for (int i = 0; i < listaNumeros.Length; i++)
+{
+    listaNumeros[i]= i + 1;
+}
+
+Console.WriteLine("Ingrese el número a buscar:");
+
+buscar = int.Parse(Console.ReadLine());
+
+if (buscar < 1 || buscar > listaNumeros.Length ) 
+{
+    Console.WriteLine("Valor no valido");
+}
+else
+{
+   posicion = Buscarnumero(listaNumeros,0,listaNumeros.Length - 1,buscar);
+}
+
+Console.WriteLine("la posición del en la array es: " + posicion);
+
+static int Buscarnumero(int[] lista, int min, int max, int? num)
+{
+
+    int med = (min + max) / 2;
+
+    if (lista[med] == num)
+    {
+        return med;
+    }
+    else
+    {
+        if (lista[med] < num)
+        {
+            min = med;
+            return Buscarnumero (lista, min, max, num);
+        }
+        else
+        {
+            max = med;
+            return Buscarnumero(lista, min, max, num);
+        }
+    }
+}
