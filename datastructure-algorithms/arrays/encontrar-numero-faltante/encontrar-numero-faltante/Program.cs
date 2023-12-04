2ea737fc0770a6d8466d1ac8e51c4c6624c2eebc
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-03 20:55:52 -0400

    create encontrar-numero-faltante

---
+++ datastructure-algorithms/arrays/encontrar-numero-faltante/encontrar-numero-faltante/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] numeros = new int[100];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (i + 1 != 72)
+    {
+        numeros[i] = i + 1;
+    }
+}
+
+int total = NumeroMaximó(numeros.Length);
+
+for (int i = 0; i < numeros.Length; i++ )
+{
+    total -= numeros[i];
+}
+
+Console.WriteLine("El número faltante es: " + total);
+
+static int NumeroMaximó(int num)
+{
+    return (num * (num + 1)) / 2;
+}
