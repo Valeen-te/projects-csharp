42e3efb8f68a319db98df79e49a7e5a6eaa7561a
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-05 09:17:33 -0400

    create torres-de-hanoi

---
+++ datastructure-algorithms/recursion/torres-de-hanoi/torres-de-hanoi/Program.cs
@@ -0,0 +1,15 @@
+﻿char[] torresHanoi = { 'A', 'B', 'C' };
+
+int numeroDiscos = 3;
+
+MoverTorres(torresHanoi[0], torresHanoi[2], torresHanoi[1], numeroDiscos);
+
+static void MoverTorres(char torreIni,char torreFin, char torreTemp, int discos)
+{
+    if(discos > 0)
+    {
+        MoverTorres(torreIni, torreTemp, torreFin, discos - 1);
+        Console.WriteLine($"Mover disco de {torreIni} a {torreFin}");
+        MoverTorres(torreTemp, torreFin, torreIni, discos - 1);
+    }
+}
