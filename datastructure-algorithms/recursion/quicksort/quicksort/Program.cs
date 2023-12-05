ed4da552e74798dc2ad94907fc34ff63fadec5c7
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-04 23:16:44 -0400

    create quicksort v1.0

---
+++ datastructure-algorithms/recursion/quicksort/quicksort/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] array = { 4, 1, 7, 3, 6, 8, 2, 9, 5 };
+
+MostrarArray(array);
+
+MoverNumeros(array, 0, array.Length - 1);
+
+MostrarArray(array);
+
+static void MoverNumeros(int[] arr, int min, int max)
+{
+    int pivote = arr[max];
+
+    int i = min;
+
+    for (int j = 0; j < arr.Length - 1; j++)
+    {
+        if (arr[j] > pivote)
+        {
+            i++;
+            int temp = arr[j];
+            arr[j] = arr[max];
+            arr[max] = temp;
+        }
+    }
+}
+
+static void MostrarArray(int[] arr)
+{
+    foreach (int i in arr)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
