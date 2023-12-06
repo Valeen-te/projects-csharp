073015de37014cb89cb8966779ec87d8af4fc743
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-06 07:40:45 -0400

    create buscar-nodo-linkedlist

---
+++ datastructure-algorithms/datastructure/buscar-nodo-linkedlist/buscar-nodo-linkedlist/Program.cs
@@ -0,0 +1,19 @@
+﻿//Crear de Linkedlist
+LinkedList<string> listaEnlazada = new();
+
+//Añadir objetos al LinkedList
+listaEnlazada.AddLast("A");
+listaEnlazada.AddLast("B");
+listaEnlazada.AddLast("C");
+
+//Buscar nodo con el método find
+LinkedListNode<string> nodo = listaEnlazada.Find("A");
+
+//Agregar un nuevo objeto despues del nodo
+listaEnlazada.AddAfter(nodo, "String");
+
+//Imprimir LinkedList
+foreach (string str in listaEnlazada)
+{
+    Console.WriteLine(str);
+}
