3f13f69958cfcdf522d46b8adaef6fcdc25e0193
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-06 08:13:36 -0400

    modified invertir-linkedlist add comments change bucle while to for rename function "CambiarNumeros" to "CambiarNodos"

--- datastructure-algorithms/datastructure/invertir-linkedlist/invertir-linkedlist/Program.cs
+++ datastructure-algorithms/datastructure/invertir-linkedlist/invertir-linkedlist/Program.cs
@@ -1,4 +1,5 @@
-﻿LinkedList<int> list = new LinkedList<int>();
+﻿//Crear LinkedList y añadir 5 objetos
+LinkedList<int> list = new LinkedList<int>();
 
 list.AddLast(5);
 list.AddLast(10);
@@ -6,42 +7,49 @@ list.AddLast(15);
 list.AddLast(20);
 list.AddLast(25);
 
+//Mostrar Linkedlist original llamando al función MostrarLinkedList
 Console.WriteLine("Linked List inicial:");
 
 MostrarLinkedList(list);
 
+//Se invierte la LinkedList llamando a la función InvertirLinkedList
 InvertirLinkedList(list);
 
+//Mostrar Linkedlist inverso
 Console.WriteLine("Linked List inverso:");
 
 MostrarLinkedList(list);
 
 static void InvertirLinkedList(LinkedList<int> lista)
 {
-
+    //Se guardan el primer y segundo nodo de la LinkedList
     LinkedListNode<int> first = lista.First;
     LinkedListNode<int> second = lista.First.Next;
 
+    /*Se guarda la longitud de la linkedlist menos uno, esto permitira 
+    mover los valores de los nodos a su posición final*/
     int repeticiones = lista.Count - 1;
 
+    /*Declaramos un buble for con el indice i igual a las repeticiones,
+    este va indicar la posicion a la que se movera el nodo*/
     for (int i = repeticiones;  i >= 0; i--)
     {
-        int j = i;
-
-        while (j != 0)
+        //Declaramos un bucle for que movera el nodo a su nueva posción 
+        for(int j = i; j > 0; j--)
         {
-
             LinkedListNode<int> temp = second;
 
-            CambiarNumeros(first, second);
+            /*Llamamos al metodo CambiarNodos, este intercambiara los valores
+            de first y second*/
+            CambiarNodos(first, second);
 
             second = second.Next;
 
             first = temp;
 
-            j--;
         }
 
+        //Reiniciamos los valores de first y second
         first = lista.First;
         second = lista.First.Next;
 
@@ -49,7 +57,8 @@ static void InvertirLinkedList(LinkedList<int> lista)
 
 }
 
-static void CambiarNumeros(LinkedListNode<int> node1, LinkedListNode<int> node2)
+//La función CambiarNodos intercambia los valores de los nodos ingresados
+static void CambiarNodos(LinkedListNode<int> node1, LinkedListNode<int> node2)
 {
     int temp = node2.Value;
 
@@ -59,6 +68,7 @@ static void CambiarNumeros(LinkedListNode<int> node1, LinkedListNode<int> node2)
 
 }
 
+//La función MostrarLinkedList imprime en consola el LinkedList ingresado
 static void MostrarLinkedList(LinkedList<int> list)
 {
     foreach (int i in list)
