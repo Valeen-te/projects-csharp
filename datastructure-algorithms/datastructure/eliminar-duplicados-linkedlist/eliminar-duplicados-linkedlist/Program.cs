35d16b101ae80e47671afdcf1051e900cda9fc16
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-07 16:56:16 -0400

    create eliminar-duplicados-linkedlist

---
+++ datastructure-algorithms/datastructure/eliminar-duplicados-linkedlist/eliminar-duplicados-linkedlist/Program.cs
@@ -0,0 +1,60 @@
+﻿LinkedList<int> lista = new();
+
+lista.AddLast(1);
+lista.AddLast(2);
+lista.AddLast(1);
+lista.AddLast(3);
+lista.AddLast(2);
+lista.AddLast(4);
+lista.AddLast(4);
+
+Console.WriteLine("Lista original: ");
+
+MostrarLista(lista);
+
+EliminarDuplicados(lista);
+
+Console.WriteLine("Eliminando duplicados: ");
+
+MostrarLista(lista);
+
+static void EliminarDuplicados(LinkedList<int> lista)
+{
+    LinkedListNode<int> nodeRef = lista.First;
+
+    int length = lista.Count;
+
+    while (nodeRef != null)
+    {
+        LinkedListNode<int> temp = lista.First;
+
+        for (int i = 0; i < length; i++)
+        {
+            if(temp != nodeRef)
+            {
+                if(temp.Value == nodeRef.Value)
+                {
+                    temp = temp.Previous;
+
+                    lista.Remove(temp.Next);
+
+                    length--;
+                }
+            }
+
+            temp = temp.Next;
+        }
+
+        nodeRef = nodeRef.Next;
+    }
+}
+
+
+static void MostrarLista(LinkedList<int> lista)
+{
+    foreach (int i in lista)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
