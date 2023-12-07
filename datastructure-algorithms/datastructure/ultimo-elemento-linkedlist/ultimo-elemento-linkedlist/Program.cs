ba2cc89638a4174666931bc385531489bffde3ad
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-07 17:22:07 -0400

    create ultimo-elemento-linkedlist

---
+++ datastructure-algorithms/datastructure/ultimo-elemento-linkedlist/ultimo-elemento-linkedlist/Program.cs
@@ -0,0 +1,37 @@
+﻿LinkedList<int> lista = new();
+
+lista.AddLast(1);
+lista.AddLast(2);
+lista.AddLast(3);
+lista.AddLast(4);
+lista.AddLast(5);
+
+Console.WriteLine("Lista: ");
+
+foreach (int i in lista)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+
+MostraUltimo(lista);
+
+static void MostraUltimo(LinkedList<int> lista)
+{
+    if (lista.Count < 1 || lista.First == null)
+    {
+        return;
+    }
+
+    LinkedListNode<int> node = lista.First;
+
+    int length = lista.Count - 1;
+
+    for (int i = 0; i < length; i++)
+    {
+        node = node.Next;
+    }
+
+    Console.WriteLine("Último elemento: " + node.Value);
+}
