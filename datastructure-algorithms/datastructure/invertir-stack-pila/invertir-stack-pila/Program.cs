2276a76f28cd878ae0adb0d12f9b863d99f1b192
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-15 11:20:47 -0400

    create invertir-stack-pila

---
+++ datastructure-algorithms/datastructure/invertir-stack-pila/invertir-stack-pila/Program.cs
@@ -0,0 +1,38 @@
+﻿Stack<int> pila = new();
+
+pila.Push(53);
+pila.Push(10);
+pila.Push(30);
+pila.Push(23);
+pila.Push(7);
+
+Console.WriteLine("Pila Original: ");
+
+MostrarStack(pila);
+
+pila = InvertirStack(pila);
+
+Console.WriteLine("Pila Inversa: ");
+
+MostrarStack(pila); 
+
+static void MostrarStack(Stack<int> p)
+{
+    foreach (int i in p)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
+
+static Stack<int> InvertirStack(Stack<int> p)
+{
+    Stack<int> temp = new();
+
+    while (p.Count > 0)
+    {
+        temp.Push(p.Pop());
+    }
+
+    return temp;
+}
