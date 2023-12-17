18afa3badf076bc6964b3728bb8b756504520227
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-16 21:48:25 -0400

    create  clasificacion-por-seleccion

---
+++ datastructure-algorithms/sorting-algorithms/clasificacion-por-seleccion/clasificacion-por-seleccion/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] Numeros = { 55, 46, 78, 89, 33, 25, 12, 5, 38 };
+
+Console.WriteLine("Array números desordenados: ");
+
+foreach(int i in Numeros)
+{
+    Console.Write(i + " ");
+}
+
+for (int i = 0; i < Numeros.Length; i++)
+{
+    int min = i;
+
+    for (int j = i + 1;  j < Numeros.Length; j++)
+    {
+        if (Numeros[min] > Numeros[j])
+        {
+            min = j;
+        }
+    }
+    int temp = Numeros[i];
+    Numeros[i] = Numeros[min];
+    Numeros[min] = temp;
+}
+
+Console.WriteLine("\nArray números ordenados: ");
+
+foreach (int i in Numeros)
+{
+    Console.Write(i + " ");
+}
