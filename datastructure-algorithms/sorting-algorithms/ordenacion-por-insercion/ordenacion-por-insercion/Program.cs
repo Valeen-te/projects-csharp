5d26fbd2a348ebbaa9467aa89b71bdc9aa2c1a19
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-17 22:06:26 -0400

    create ordenacion-por-insercion

---
+++ datastructure-algorithms/sorting-algorithms/ordenacion-por-insercion/ordenacion-por-insercion/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] arreglo = { 15, 12, 8, 18, 5, 2, 7 };
+
+Console.WriteLine("Arreglo original: ");
+
+MostrarArray(arreglo);
+
+InsertionSort(arreglo);
+
+Console.WriteLine("Arreglo ordenado: ");
+
+MostrarArray(arreglo);
+
+static void MostrarArray(int[] arr)
+{
+    foreach (int i in arr)
+    {
+        Console.Write(i + " ");
+    }
+
+    Console.WriteLine();
+}
+
+static void InsertionSort(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int valor = arr[i];
+        int j = i - 1;
+        while (j != -1 && arr[j] > valor)
+        {
+            arr[j + 1 ] = arr[j];
+            j--;
+            arr[j + 1] = valor;
+        }
+
+        arr[j + 1] = valor;
+    }
+}
