20c642c8de787d00d61014b201b49fde6a46ba45
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2023-12-26 22:14:25 -0400

    create merge-sort

---
+++ datastructure-algorithms/sorting-algorithms/merge-sort/merge-sort/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 4, 6, 9, 1, 7, 5, 3, 0, 2, 8 };
+
+MergeSort(array, 0, array.Length);
+
+static void MergeSort(int[] arr, int min, int max)
+{
+    int cut = max / 2;
+
+    if (cut < min)
+    {
+        MergeSort(arr, min, cut);
+    }
+}
