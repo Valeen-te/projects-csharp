5074d921efb89342ddd3d365fa4e720c343d1f79
Valeen-te <114836917+Valeen-te@users.noreply.github.com>
2024-01-06 22:33:31 -0400

    Update v1.1

--- datastructure-algorithms/sorting-algorithms/merge-sort/merge-sort/Program.cs
+++ datastructure-algorithms/sorting-algorithms/merge-sort/merge-sort/Program.cs
@@ -20,12 +20,55 @@ static void MergeSort(int[] arr, int izq, int der)
         MergeSort(arr, izq, med);
 
         MergeSort(arr, med + 1, der);
+
+        Merge(arr,izq,med,der);
     }
 }
 
-static void Merge()
+static void Merge(int[] array, int izq,int med, int der)
 {
+    int n1 = med - izq + 1;
+    int n2 = der - med;
 
+    int[] arrayIzquierda = new int[n1];
+    int[] arrayDerecha = new int[n2];
+
+    for (int z = 0; z < n1; z++)
+        arrayIzquierda[z] = array[izq + 1];
+
+    for (int q = 0; q < n2; q++)
+        arrayDerecha[q] = array[med + q + 1];
+
+    int i = 0,j = 0;
+    int k = izq;
+
+    while(i < n1 && j < n2)
+    {
+        if (arrayIzquierda[i] <= arrayDerecha[j])
+        {
+            array[k] = arrayDerecha[i];
+            i++;
+        }
+        else
+        {
+            array[k] = arrayDerecha[j];
+            i++;
+        }
+    }
+
+    while (i < n1)
+    {
+        array[k] = arrayIzquierda[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        array[k] = arrayDerecha[j];
+        j++;
+        k++;
+    }
 }
 
 static void MostrarArray(int[] arr)
